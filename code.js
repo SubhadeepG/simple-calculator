fb7fbd2d74b3c8e198ec85fbc7f657501633bef3
Subhadeep Pal <subhadeeppal63@gmail.com>
2026-07-18 14:13:38 +0530

    Added Sum and Subtract function to the Calculator.
    The calculator can now add and subtract numbers.

--- code.js
+++ code.js
@@ -1,9 +1,26 @@
 const buttonContainer = document.querySelector(".all-buttons");
 const output = document.querySelector("#screen-output");
 
-let firstOperand;
-let secondOperand;
-let operator;
+let firstOperand = 0;
+let secondOperand = 0;
+let operator = "";
+
+function operate() {
+    const num1 = Number(firstOperand);
+    const num2 = Number(secondOperand);
+    let result;
+    if(operator === "+") result = num1 + num2;
+    else if(operator === "-") result = num1 - num2;
+    else if(operator === "&#215") result = num1 * num2;
+    else if(operator === "&#247") result = num1 / num2; 
+    else if(operator === "&#37") result = num1 % num2;
+    else console.log("error: operator not found");
+    secondOperand = 0;
+    operator = "";
+    firstOperand = result;
+    output.textContent = result;
+    return result;
+}
 
 buttonContainer.addEventListener("click", function(event) {
     // check if the user click is on the button and not the container
@@ -14,20 +31,64 @@ buttonContainer.addEventListener("click", function(event) {
     const button = event.target;
     const buttonValue = button.textContent;
     console.log("button clicked: " + buttonValue);
-});
 
-function add(a, b) {
-    return a + b;
-}
-function subtract(a,b) {
-    return;
-}
-function multiply(a,b) {
-    return;
-}
-function divide(a,b) {
-    return;
-}
-function operate(operator, num1, num2) {
-    return;
-}
+    // if user input a number
+    if(button.classList.contains("digits")) {
+        // if operator is null, input the first number
+        if(!operator) {
+            firstOperand = firstOperand + buttonValue;
+            if(firstOperand[0] === "0") {
+                firstOperand = firstOperand.slice(1);
+            }
+            output.textContent = firstOperand;
+            return;
+        }
+        else {
+            // input the second number
+            secondOperand = secondOperand + buttonValue;
+            if(secondOperand[0] === "0") {
+                secondOperand = secondOperand.slice(1);
+            }
+            output.textContent = firstOperand + " " + operator + " " + secondOperand;
+            return;
+        }
+    }
+    // if user input a operator (+, -, *, /, %)
+    if(button.classList.contains("operator")) {
+        if(!operator) {
+            if(!firstOperand) {
+                console.log("error: first number is not given but user input a operator");
+                return;
+            }
+            operator = buttonValue;
+            output.textContent = firstOperand + " " + operator;
+            return;
+        }
+        else if(secondOperand && operator) {
+            const result = operate();
+            output.textContent = result + " " + buttonValue;
+            operator = buttonValue;
+            return;
+        }
+    }
+    // if the operator is AC, C or = 
+    if(button.classList.contains("operator-b")) {
+        if(buttonValue === "=") {
+            if(firstOperand && operator && secondOperand) {
+                operate();
+                return;
+            }
+            else {
+                console.log("error: user pressed = but number and operator not given");
+                return;
+            }
+        }
+        if(buttonValue === "AC") {
+            firstOperand = 0;
+            operator = "";
+            secondOperand = 0;
+            output.textContent = 0;
+            return;
+        }
+    }
+});
